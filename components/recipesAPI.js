ffcd327be1977b55575c285f4c38523e47b94fd4
jramos-tech <jramos@student.neumont.edu>
2024-02-13 20:24:30 -0700

    fixed project

--- components/recipesAPI.js
+++ components/recipesAPI.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
 
 let arrayName = []
 let arrayCuisine = []
@@ -22,52 +21,35 @@ const RecipesFormat = ({ name }) => {
     );
   };
 export default function RecipesHook() {
-    const [ingridients, setIngridients] = useState('chicken')
-    const [totalResults, settotalResults] = useState(0);
-    const grabRecipe = () => {
-        const APIKEY = '644cd0c5013146b0bb6021ab0c0027f2'
-        const BASE_URL = 'https://spoonacular.com/recipes/complexSearch?'
-        const PARAMS = `&query=${ingridient}`
-        let FETCH_URL = `${BASE_URL}apiKey=${APIKEY}${PARAMS}`
-        console.log(FETCH_URL)
-        fetch(FETCH_URL, { method: 'GET' })
+    const [ingredients, setIngredients] = useState('american');
+    const [recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        grabRecipes();
+    }, [ingredients]);
+
+    const grabRecipes = () => {
+        const APIKEY = '644cd0c5013146b0bb6021ab0c0027f2';
+        const BASE_URL = 'https://api.spoonacular.com/recipes/complexSearch';
+        const PARAMS = `?apiKey=${APIKEY}&cuisine=${ingredients}`;
+        const FETCH_URL = `${BASE_URL}${PARAMS}`;
+
+        fetch(FETCH_URL)
             .then((response) => response.json())
             .then((json) => {
-                for (let i = 0; i < json.length; i += 5) { arrayName[`name_${i}`] = json.list[i].dt_txt.substring(0, 10) }
-                settotalResults(json.list.length)
+                if (json.results && json.results.length > 0) {
+                    const recipeNames = json.results.map(recipe => recipe.title);
+                    setRecipes(recipeNames);
+                }
             })
-    }
-        useEffect(() => {
-            console.log(ingridients);
-            grabRecipe();
-        }, [ingridients]);
-        let recipes = [];
-        for (let i = 0; i < totalResults; i += 8) {
-            recipes.push(
-                <RecipesFormat
-                    key={i}
-                    name={arrayName[`name_${i}`]}
-                    
-                />
-            );
-    }
+            .catch((error) => console.error('Error fetching recipes:', error));
+    };
+
     return (
         <View>
-        <Text>Recipes </Text>
-        <Picker
-            selectedValue={ingridients}
-            style={{ heigh: 50, width: 200 }}
-            onValueChange={(val, idx) => {
-              setIngridients(val); //trigger useEffect which calls grabWeather() when variable changes
-            }}>
-            <Picker.Item label="Chicken" value="chicken" />
-            <Picker.Item label="Shrimp" value="shrimp" />
-            <Picker.Item label="Steak" value="steak" />
-            <Picker.Item label="Fish" value="fish"/>
-        </Picker>
-        {recipes}
+            {recipes.map((recipe, index) => (
+                <RecipesFormat key={index} name={recipe} />
+            ))}
         </View>
-);
-    }
-
-
+    );
+}
