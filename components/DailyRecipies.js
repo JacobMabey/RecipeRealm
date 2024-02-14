6b590a4f4a3c7839a9ede4741328e0365b0d93e3
Mind-Mage <126829895+Mind-Mage@users.noreply.github.com>
2024-02-13 19:29:36 -0700

    Create DailyRecipies
    This is the start for basic temp data handling

---
+++ components/DailyRecipies.js
@@ -0,0 +1,86 @@
+//these will hold the temp images
+BreakfeastImages = [];
+LunchImages = [];
+DinnerImages = [];
+DessertImages = [];
+// these will object refs to recpies holding the temp data
+Breakfeast = [];
+Lunch = [];
+Dinner = [];
+Dessert = [];
+
+//these should be the basic for the images 
+function getBreakfeastItem (entry)
+{
+    return BreakfeastImages[entry];
+}
+
+function addBreakfeastItem (item)
+{
+    BreakfeastImages.Push(item);
+}
+function getLunchItem (entry)
+{
+    return LunchImages[entry];
+}
+
+function addLuchItem (item)
+{
+    LunchImages.Push(item);
+}
+function getDinnerItem (entry)
+{
+    return DinnerImages[entry];
+}
+
+function addDinnerItem (item)
+{
+    DinnerImages.Push(item);
+}
+function getDessertItem (entry)
+{
+    return DessertImages[entry];
+}
+
+function addDessertItem (item)
+{
+    DessertImages.Push(item);
+}
+
+// need to make the recpie object then I'll come back and rewrite these
+function getBreakfeastItem (entry)
+{
+    return Breakfeast[entry];
+}
+
+function addBreakfeastItem (item)
+{
+    Breakfeast.Push(item);
+}
+function getLunchItem (entry)
+{
+    return Lunch[entry];
+}
+
+function addLuchItem (item)
+{
+    Lunch.Push(item);
+}
+function getDinnerItem (entry)
+{
+    return Dinner[entry];
+}
+
+function addDinnerItem (item)
+{
+    Dinner.Push(item);
+}
+function getDessertItem (entry)
+{
+    return Dessert[entry];
+}
+
+function addDessertItem (item)
+{
+    Breakfeast.Push(item);
+}
